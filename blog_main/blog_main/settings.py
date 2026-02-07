fb6af0b9d788e218d058eedd561d5ba46577f737
aglaxman <laxmannaagoudar8@gmail.com>
2026-02-07 12:32:35 +0530

    commit

--- blog_main/blog_main/settings.py
+++ blog_main/blog_main/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "blogs",
 ]
 
 MIDDLEWARE = [
@@ -120,3 +121,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / 'static',
 ]
+
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
