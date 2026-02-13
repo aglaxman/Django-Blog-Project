9124260ac141aeb7cae1952848edd77eb5aea79e
aglaxman <laxmannaagoudar8@gmail.com>
2026-02-13 14:46:40 +0530

    commit

--- blog_main/blogs/views.py
+++ blog_main/blogs/views.py
@@ -13,13 +13,13 @@ def posts_by_category(req, category_id):
 
     posts = Blog.objects.filter(status = 'Published' ,category = category_id)
 
-    #! try except when u want to do some costum action
+    # try except when u want to do some costum action
     # try:
     #     category = Category.objects.get(pk = category_id)
     # except:
     #     return redirect('home')
 
-    #! use get_object_or_404 when u wnat to use the page not found page if the cateogry doesnot exit 
+    # use get_object_or_404 when u wnat to use the page not found page if the cateogry doesnot exit 
     category = get_object_or_404(Category, pk = category_id)
 
 
